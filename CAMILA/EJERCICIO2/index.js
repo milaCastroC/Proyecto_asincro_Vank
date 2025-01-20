034bfba4ba401ee27ef0f4f710ec804323bd1a4b
Maria Camila Castro <castro.maria.5191@eam.edu.co>
2025-01-20 18:19:28 -0500

    EJERCICIO 2

--- CAMILA/EJERCICIO2/index.js
+++ CAMILA/EJERCICIO2/index.js
@@ -1,83 +1,137 @@
 require('dotenv').config();
+const readLine = require( 'readline/promises'); 
 
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
 const HUGGING_API_KEY = process.env.HUGGING_API_KEY;
+const COHERE_API_KEY = process.env.COHERE_API_KEY;
 
 
-async function testAPIKeyOpenIA() {
+async function resumirHugging(texto) {
+    const response = await fetch('https://api-inference.huggingface.co/models/t5-base', {
+        method: 'POST',
+        headers:{
+            'Authorization': `Bearer ${HUGGING_API_KEY}`,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ inputs: `Resume el siguiente texto: ${texto}` })
+    });
 
-    try{
-        const response = await fetch('https://api.openai.com/v1/models', {
-            method: 'GET',
-            headers: {
-                'Authorization': `Bearer ${OPENAI_API_KEY}`,
-                'Content-Type': 'application/json'
-            }
-        });
+    if (!response.ok) {
+        const errorText = await response.text();
+        throw new Error(`Error en Hugging Face API: ${response.status} - ${errorText}`);
+    }
 
-        if(response.ok){
-            const data = await response.json();
-            console.log('Conexión exitosa: ', data);
-        }else{
-            console.error('Error: ', response.status, await response.text());
-        }
-    }catch(error){
-        console.log("Error al conectar con OpenAI: ", error);
+    const data = await response.json();
+    if (Array.isArray(data) && data.length > 0 && data[0].summary_text) {
+        return data[0].summary_text.trim();
+    } else {
+        throw new Error('La respuesta de Hugging Face no contiene "summary_text".');
     }
 }
-
-testAPIKeyOpenIA();
 
 const API_AI_URL = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent';
 
-async function testAPIKeyGemini() {
+async function resumirGemini(texto) {
+    const response = await fetch(`${API_AI_URL}?key=${GEMINI_API_KEY}`, {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+        },
+        body: JSON.stringify({
+            contents: [{
+                parts: [{ text: `Resume el siguiente texto: \n\n${texto}`}],
+            }],
+        }),
+    });
+     if (!response.ok) {
+        const errorText = await response.text();
+        throw new Error(`Error en Gemini API: ${response.status} - ${errorText}`);
+    }
+
+    const data = await response.json();
+
+    if (data.candidates && data.candidates.length > 0) {
+        const content = data.candidates[0].content;
+
+        if (content && content.parts && content.parts.length > 0) {
+            console.log(content.parts[0].text.trim()); 
+        } else {
+            throw new Error('No se encontró contenido en "parts".');
+        }
+    } else {
+        throw new Error('La respuesta de Gemini no contiene "candidates".');
+    } 
+}
+
+async function resumirCohere(texto) {
     try {
-        const response = await fetch(`${API_AI_URL}?key=${GEMINI_API_KEY}`, {
+        const response = await fetch('https://api.cohere.ai/v1/generate', {
             method: 'POST',
             headers: {
+                'Authorization': `Bearer ${COHERE_API_KEY}`,
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify({
-                "contents": [{
-                    "parts":[{"text": "Write a story about a magic backpack."}]
-                 }],
+                prompt: `Resume el siguiente texto y responde siempre en español:  ${texto}`,
+                max_tokens: 200,
+                temperature: 0.7,
             }),
         });
 
-        if (response.ok) {
-            const data = await response.json();
-            console.log('Conexión exitosa:',  data);
-        } else {
-            console.error('Error:', response.status, await response.text());
+        if (!response.ok) {
+            throw new Error(`Error HTTP: ${response.status}`);
         }
+
+        const data = await response.json();
+        const generatedText = data.generations[0].text || 'Texto no disponible'; // Estructura esperada
+        return generatedText;
     } catch (error) {
-        console.error('Error al conectar con Gemini:', error);
+        console.error('Error al llamar a la API de COHERE:', error);
+        throw error;
     }
 }
 
-testAPIKeyGemini();
 
-async function testAPIKeyHugging() {
+// async function resumirOpenIa(texto) {
+//     const response = await fetch('https://api.openai.com/v1/completions',{
+//         method: 'POST',
+//         headers: {
+//             'Authorization' : `Bearer ${OPENAI_API_KEY}`,
+//             'Content-type': 'application/json',
+//         },
+//         body: JSON.stringify({
+//             model: 'gpt-3.5-turbo',
+//             prompt: `Resume el siguiente texto: \n\n${texto}`,
+//             // max_tokens: 100,
+//         }),
+//     });
 
-    try{
-        const response = await fetch('https://api-inference.huggingface.co/models/facebook/bart-large-cnn', {
-            method: 'GET',
-            headers: {
-                'Authorization': `Bearer ${HUGGING_API_KEY}`,
-                'Content-Type': 'application/json'
-            }
-        });
+//     if (!response.ok) {
+//         const errorText = await response.text();
+//         throw new Error(`Error en OpenAI API: ${response.status} - ${errorText}`);
+//     }
 
-        if(response.ok){
-            const data = await response.json();
-            console.log('Conexión exitosa: ', data);
-        }else{
-            console.error('Error: ', response.status, await response.text());
-        }
-    }catch(error){
-        console.log("Error al conectar con OpenAI: ", error);
+//     const data = await response.json();
+//     if (data.choices && data.choices.length > 0) {
+//         return data.choices[0].text.trim();
+//     } else {
+//         throw new Error('La respuesta de OpenAI no contiene "choices".');
+//     }
+// }
+
+// resumirOpenIa("La inteligencia artificial (IA) se refiere a la simulación de procesos de inteligencia humana por parte de sistemas informáticos. Estos procesos incluyen el aprendizaje, el razonamiento y la autocorrección. En la actualidad, la IA está presente en diversas aplicaciones, como asistentes virtuales, vehículos autónomos, diagnóstico médico, y análisis de datos. Su desarrollo ha generado debates sobre su impacto en el empleo, la privacidad y la ética, lo que subraya la importancia de establecer marcos regulatorios adecuados para su implementación responsable.");
+
+
+async function generarResumen(texto) {
+
+    try {
+        const resumen = await Promise.race([resumirGemini(texto),resumirHugging(texto),resumirCohere(texto),]);
+        console.log('Resumen generado: ', resumen);
+    } catch (error) {
+        console.error('Error al generar resumen: ', error.message);
     }
 }
 
-testAPIKeyHugging();
+
+generarResumen("La inteligencia artificial (IA) se refiere a la simulación de procesos de inteligencia humana por parte de sistemas informáticos. Estos procesos incluyen el aprendizaje, el razonamiento y la autocorrección. En la actualidad, la IA está presente en diversas aplicaciones, como asistentes virtuales, vehículos autónomos, diagnóstico médico, y análisis de datos. Su desarrollo ha generado debates sobre su impacto en el empleo, la privacidad y la ética, lo que subraya la importancia de establecer marcos regulatorios adecuados para su implementación responsable.");
