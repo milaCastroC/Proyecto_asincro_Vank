c98a67a8a6e853bf79d7d1e99a8d828562b84052
Mariluz Calderon <calderon.mariluz.8892@eam.edu.co>
2025-01-20 19:38:45 -0500

    FIX hugging face API conection - EJERCICIO2

--- CAMILA/EJERCICIO2/index.js
+++ CAMILA/EJERCICIO2/index.js
@@ -1,14 +1,12 @@
 require('dotenv').config();
-const readLine = require( 'readline/promises'); 
 
-const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
 const HUGGING_API_KEY = process.env.HUGGING_API_KEY;
 const COHERE_API_KEY = process.env.COHERE_API_KEY;
 
 
 async function resumirHugging(texto) {
-    const response = await fetch('https://api-inference.huggingface.co/models/t5-base', {
+    const response = await fetch('https://api-inference.huggingface.co/models/facebook/bart-large-cnn', {
         method: 'POST',
         headers:{
             'Authorization': `Bearer ${HUGGING_API_KEY}`,
@@ -24,7 +22,7 @@ async function resumirHugging(texto) {
 
     const data = await response.json();
     if (Array.isArray(data) && data.length > 0 && data[0].summary_text) {
-        return data[0].summary_text.trim();
+        return {text: data[0].summary_text.trim(), ia_model: 'BART-LARGE-CNN (extraido de HUGGING FACE)'} 
     } else {
         throw new Error('La respuesta de Hugging Face no contiene "summary_text".');
     }
@@ -55,7 +53,7 @@ async function resumirGemini(texto) {
         const content = data.candidates[0].content;
 
         if (content && content.parts && content.parts.length > 0) {
-            console.log(content.parts[0].text.trim()); 
+            return { text: content.parts[0].text.trim(), ia_model: 'GEMINI'}; 
         } else {
             throw new Error('No se encontró contenido en "parts".');
         }
@@ -73,7 +71,7 @@ async function resumirCohere(texto) {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify({
-                prompt: `Resume el siguiente texto y responde siempre en español:  ${texto}`,
+                prompt: `Summarize in Spanish the next text:   ${texto}`,
                 max_tokens: 200,
                 temperature: 0.7,
             }),
@@ -84,54 +82,49 @@ async function resumirCohere(texto) {
         }
 
         const data = await response.json();
-        const generatedText = data.generations[0].text || 'Texto no disponible'; // Estructura esperada
-        return generatedText;
+        const generatedText = `COHERE: ` + data.generations[0].text || 'Texto no disponible'; // Estructura esperada
+        return {text: generatedText, ia_model: 'COHERE'};
     } catch (error) {
         console.error('Error al llamar a la API de COHERE:', error);
         throw error;
     }
 }
 
-
-// async function resumirOpenIa(texto) {
-//     const response = await fetch('https://api.openai.com/v1/completions',{
-//         method: 'POST',
-//         headers: {
-//             'Authorization' : `Bearer ${OPENAI_API_KEY}`,
-//             'Content-type': 'application/json',
-//         },
-//         body: JSON.stringify({
-//             model: 'gpt-3.5-turbo',
-//             prompt: `Resume el siguiente texto: \n\n${texto}`,
-//             // max_tokens: 100,
-//         }),
-//     });
-
-//     if (!response.ok) {
-//         const errorText = await response.text();
-//         throw new Error(`Error en OpenAI API: ${response.status} - ${errorText}`);
-//     }
-
-//     const data = await response.json();
-//     if (data.choices && data.choices.length > 0) {
-//         return data.choices[0].text.trim();
-//     } else {
-//         throw new Error('La respuesta de OpenAI no contiene "choices".');
-//     }
-// }
-
-// resumirOpenIa("La inteligencia artificial (IA) se refiere a la simulación de procesos de inteligencia humana por parte de sistemas informáticos. Estos procesos incluyen el aprendizaje, el razonamiento y la autocorrección. En la actualidad, la IA está presente en diversas aplicaciones, como asistentes virtuales, vehículos autónomos, diagnóstico médico, y análisis de datos. Su desarrollo ha generado debates sobre su impacto en el empleo, la privacidad y la ética, lo que subraya la importancia de establecer marcos regulatorios adecuados para su implementación responsable.");
-
-
 async function generarResumen(texto) {
 
     try {
-        const resumen = await Promise.race([resumirGemini(texto),resumirHugging(texto),resumirCohere(texto),]);
-        console.log('Resumen generado: ', resumen);
+        const response = await Promise.race([resumirGemini(texto), resumirHugging(texto), resumirCohere(texto)]);
+        console.log(`\n ***** EL GANADOR ES: ${response.ia_model}***** \n`);
+        console.log(`RESUMEN: ${response.text}`);
+
+        return response;
     } catch (error) {
         console.error('Error al generar resumen: ', error.message);
     }
 }
 
+//generarResumen("La inteligencia artificial se refiere a la simulación de procesos de inteligencia humana por parte de sistemas informáticos. Estos procesos incluyen el aprendizaje, el razonamiento y la autocorrección. En la actualidad, la IA está presente en diversas aplicaciones, como asistentes virtuales, vehículos autónomos, diagnóstico médico, y análisis de datos. Su desarrollo ha generado debates sobre su impacto en el empleo, la privacidad y la ética, lo que subraya la importancia de establecer marcos regulatorios adecuados para su implementación responsable.");
 
-generarResumen("La inteligencia artificial (IA) se refiere a la simulación de procesos de inteligencia humana por parte de sistemas informáticos. Estos procesos incluyen el aprendizaje, el razonamiento y la autocorrección. En la actualidad, la IA está presente en diversas aplicaciones, como asistentes virtuales, vehículos autónomos, diagnóstico médico, y análisis de datos. Su desarrollo ha generado debates sobre su impacto en el empleo, la privacidad y la ética, lo que subraya la importancia de establecer marcos regulatorios adecuados para su implementación responsable.");
+generarResumen(`
+    Jaimito, el cartero, cuyo nombre secular es Jaime Garabito, es un personaje de la serie de televisión El Chavo del Ocho, interpretado por Raúl «Chato» Padilla (quien falleció en 1994).
+    Hizo su primera aparición en 1979 junto a Doña Nieves, tras la ausencia de Quico y Don Ramón, convirtiéndose en un personaje permanente hasta el fin del programa en 1980 y durante su continuación en 1982 para numerosos episodios en el programa Chespirito hasta su definitivo final en 1992.
+    Apariencia: Tiene cabello blanco, largo y descuidado, ya que se le ve con la gorra puesta.
+    Tiene un bigote también blanco con el cual resalta sus muecas y gesticulaciones.
+    Usa lentes pequeños y redondos, con un traje azul grisáceo de cartero, un bolso en el que lleva las cartas y un pañuelo en el cuello, con botas negras, un gorro negro y un cinturón.
+    En un episodio de la versión animada se mostró con un pijama con sobres.
+`)
+
+// generarResumen(`
+//     La vaquita marina o cochito (Phocoena sinus) es una especie de cetáceo odontoceto de la familia Phocoenidae, una de las siete especies de marsopa.
+//     Mide 150 cm de largo y pesa hasta 50 kg. Tiene una distribución muy restringida. También es considerada una especie endémica de México en Baja California.
+//     Sus poblaciones han disminuido durante las pasadas décadas al punto de ser considerada en grave peligro de extinción. En el 2015, la población total era de noventa y siete individuos.
+//     En 2017 perdió el 67% de su población, quedando con menos de cuarenta individuos. A finales de 2018 y principios de 2019, se estimaba la existencia de entre diez y quince. Según WWF, se está extinguiendo.
+//     Se han puesto en marcha medidas de conservación, pero la especie se considera en peligro inminente de extinción.
+//     El 18 de octubre de 2017 fue rescatada una vaquita marina de seis meses de edad; sin embargo, fue devuelta al mar por recomendación de los veterinarios expertos, quienes consideraron que la cría no podía estar separada de su madre.
+//     El 10 de noviembre de ese mismo año, científicos del Programa VaquitaCPR dieron a conocer resultados de operaciones en campo. Reportaron el avistamiento de individuos en ocho de trece días de labor en el mar. Registraron treinta y dos avistamientos, incluyendo probables individuos repetidos en el curso de un día.
+//     Es importante aclarar que estos avistamientos no representan un estimado de la población de vaquitas. Los eventos de avistamientos incluyeron de una a tres vaquitas a la vez, con promedio de dos vaquitas por grupo.
+//     En marzo de 2018, la situación se ha vuelto crítica y, según Andrea Crosta, de la organización protectora de animales Elephant Action League, solamente se han podido detectar doce individuos nadando en el golfo de California, junto a la península de Baja California.
+
+// `)
+
+
